fadb0441457ac70db9abdfbfb95eef677e4d93ac
Steve Gasson <sgassondev21@gmail.com>
2022-09-19 16:45:50 +0200

    migration

---
+++ app/Models/Item.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Item extends Model
+{
+    use HasFactory;
+
+
+    public function carts()
+    {
+        return $this->belongsToMany(Cart::class, 'items_carts', 'item_id', 'cart_id');
+    }
+
+    public function orders()
+    {
+        return $this->belongsToMany(Order::class, 'orders_items', 'order_id', 'item_id');
+    }
+}
---
+++ database/migrations/2022_09_19_142320_create_carts_items_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('carts_items', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId('cart_id')->constrained('carts','id');
+            $table->foreignId('item_id')->constrained('items','id');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('carts_items');
+    }
+};
