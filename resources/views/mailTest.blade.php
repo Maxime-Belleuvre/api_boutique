74b3964114e0899a2588fd6827778fe6dbca8eca
Maxime-Belleuvre <maximebelleuvre14200@gmail.com>
2022-09-22 10:23:19 +0200

    Update mailTest.blade.php

--- resources/views/mailTest.blade.php
+++ resources/views/mailTest.blade.php
@@ -3,7 +3,7 @@
         <?php
         require '../vendor/autoload.php';
         use \Mailjet\Resources;
-        $mj = new \Mailjet\Client('****************************1234','****************************abcd',true,['version' => 'v3.1']);
+        $mj = new \Mailjet\Client('truc','bidule',true,['version' => 'v3.1']);
         $body = [
           'Messages' => [
             [
